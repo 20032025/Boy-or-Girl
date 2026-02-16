53670dcaa578b13b66d6a6c9d157e57f377c4eb3
Soukaina Chkirid <soukainayoussefchkirid@gmail.com>
2026-02-16 12:51:01 +0100

    Remove baby feet emoji from circle
    Clear the innerHTML of the circle element.

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ if(circle.classList.contains("done")) return;
 circle.classList.add("done");
 
 /* show baby feet */
-circle.innerHTML = "👣";
+circle.innerHTML = "";
 
 /* last circle = real gender */
 if(index === 8){
